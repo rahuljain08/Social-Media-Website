33be42716df687b82ed946ff3a71a770668d5efa
rahuljain08 <70151173+rahuljain08@users.noreply.github.com>
2021-07-23 11:23:57 +0530

    Update settings.py

--- foxy/settings.py
+++ foxy/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '1@iuv^5zt_g*#b@aetjk&=&tvghgjfr3r!35mjw8$#4#(zj#@4'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
